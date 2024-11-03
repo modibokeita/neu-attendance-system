c6f3d93c772795929e6c2977fc7ba6423c20f4d0
modibo keita <93909081keita@gmail.com>
2024-11-03 21:58:13 +0200

    some changes

--- DATABASE/attendance_system.sql
+++ DATABASE/attendance_system.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 02, 2024 at 11:45 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Hôte : 127.0.0.1
+-- Généré le : dim. 03 nov. 2024 à 20:27
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `attendancemsystem`
+-- Base de données : `attendance_system`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbladmin`
+-- Structure de la table `tbladmin`
 --
 
 CREATE TABLE `tbladmin` (
@@ -36,7 +36,7 @@ CREATE TABLE `tbladmin` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `tbladmin`
+-- Déchargement des données de la table `tbladmin`
 --
 
 INSERT INTO `tbladmin` (`Id`, `firstName`, `lastName`, `emailAddress`, `password`) VALUES
@@ -45,7 +45,7 @@ INSERT INTO `tbladmin` (`Id`, `firstName`, `lastName`, `emailAddress`, `password
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblattendance`
+-- Structure de la table `tblattendance`
 --
 
 CREATE TABLE `tblattendance` (
@@ -58,20 +58,64 @@ CREATE TABLE `tblattendance` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `tblattendance`
+-- Déchargement des données de la table `tblattendance`
 --
 
 INSERT INTO `tblattendance` (`attendanceID`, `studentRegistrationNumber`, `course`, `attendanceStatus`, `dateMarked`, `unit`) VALUES
-(478, 'CIT-222-003-2020', 'BCT', 'Absent', '2024-05-02', 'BCT 2411'),
-(479, 'CIT-222-002-2020', 'BCT', 'Absent', '2024-05-02', 'BCT 2411'),
-(480, 'CIT-222-001-2020', 'BCT', 'Absent', '2024-05-02', 'BCT 2411'),
-(481, 'CIT-222-005-2020', 'BCT', 'present', '2024-05-02', 'BCT 2411'),
-(482, 'CIT-222-004-2020', 'BCT', 'Absent', '2024-05-02', 'BCT 2411');
+(493, 'CIT-222-006-2020', 'BCT', 'present', '2024-11-01', 'CIA 2103'),
+(494, 'CIT-2020-39-90', 'BSE', 'Absent', '2024-11-02', 'BCT 2411'),
+(496, 'CIT-222-002-2020', 'BCT', 'Absent', '2024-11-02', 'BCT 2409'),
+(499, 'CIT-222-004-2020', 'BCT', 'Absent', '2024-11-02', 'BCT 2409'),
+(500, 'CIT-222-006-2020', 'BCT', 'present', '2024-11-02', 'BCT 2409'),
+(501, 'CIT-2020-39-90', 'BSE', 'Absent', '2024-11-03', 'BCT 2411'),
+(502, 'CIT-222-003-2020', 'BCT', 'Absent', '2024-11-03', 'BCT 2409'),
+(503, 'CIT-222-002-2020', 'BCT', 'Absent', '2024-11-03', 'BCT 2409'),
+(507, 'CIT-222-006-2020', 'BCT', 'present', '2024-11-03', 'BCT 2409'),
+(508, 'CIT-2020-39-90', 'BSE', 'Absent', '2024-11-03', 'BCT 2411'),
+(509, '202234640', 'BSE', 'Absent', '2024-11-03', 'BCT 2411'),
+(510, 'CIT-2020-39-90', 'BSE', 'Absent', '2024-11-03', 'BCT 2411'),
+(511, '202234640', 'BSE', 'present', '2024-11-03', 'BCT 2411'),
+(512, 'CIT-2020-39-90', 'BSE', 'Absent', '2024-11-03', 'BCT 2411'),
+(513, '202234640', 'BSE', 'present', '2024-11-03', 'BCT 2411'),
+(514, 'CIT-2020-39-90', 'BSE', 'Absent', '2024-11-03', 'BCT 2411'),
+(515, '202234640', 'BSE', 'present', '2024-11-03', 'BCT 2411'),
+(516, 'CIT-2020-39-90', 'BSE', 'Absent', '2024-11-03', 'BCT 2411'),
+(517, '202234640', 'BSE', 'present', '2024-11-03', 'BCT 2411'),
+(518, 'CIT-2020-39-90', 'BSE', 'Absent', '2024-11-03', 'BCT 2411'),
+(519, '202234640', 'BSE', 'present', '2024-11-03', 'BCT 2411'),
+(520, 'CIT-2020-39-90', 'BSE', 'Absent', '2024-11-03', 'BCT 2411'),
+(521, '202234640', 'BSE', 'present', '2024-11-03', 'BCT 2411'),
+(522, 'CIT-2020-39-90', 'BSE', 'Absent', '2024-11-03', 'BCT 2411'),
+(523, '202234640', 'BSE', 'present', '2024-11-03', 'BCT 2411'),
+(524, 'CIT-2020-39-90', 'BSE', 'Absent', '2024-11-03', 'BCT 2411'),
+(525, '202234640', 'BSE', 'present', '2024-11-03', 'BCT 2411'),
+(526, 'CIT-222-003-2020', 'BCT', 'Absent', '2024-11-03', 'BCT 2411'),
+(527, 'CIT-222-002-2020', 'BCT', 'Absent', '2024-11-03', 'BCT 2411'),
+(528, 'CIT-222-001-2020', 'BCT', 'Absent', '2024-11-03', 'BCT 2411'),
+(529, 'CIT-222-005-2020', 'BCT', 'Absent', '2024-11-03', 'BCT 2411'),
+(530, 'CIT-222-004-2020', 'BCT', 'Absent', '2024-11-03', 'BCT 2411'),
+(531, 'CIT-222-006-2020', 'BCT', 'present', '2024-11-03', 'BCT 2411'),
+(532, 'CIT-2020-39-90', 'BSE', 'Absent', '2024-11-03', 'BCT 2411'),
+(533, '202234640', 'BSE', 'present', '2024-11-03', 'BCT 2411'),
+(534, 'CIT-222-003-2020', 'BCT', 'Absent', '2024-11-03', 'BCT 2409'),
+(535, 'CIT-222-002-2020', 'BCT', 'Absent', '2024-11-03', 'BCT 2409'),
+(536, 'CIT-222-001-2020', 'BCT', 'Absent', '2024-11-03', 'BCT 2409'),
+(537, 'CIT-222-005-2020', 'BCT', 'Absent', '2024-11-03', 'BCT 2409'),
+(538, 'CIT-222-004-2020', 'BCT', 'Absent', '2024-11-03', 'BCT 2409'),
+(539, 'CIT-222-006-2020', 'BCT', 'present', '2024-11-03', 'BCT 2409'),
+(540, 'CIT-222-007-2020', 'BCT', 'present', '2024-11-03', 'BCT 2409'),
+(541, 'CIT-222-003-2020', 'BCT', 'Absent', '2024-11-03', 'BCT 2411'),
+(542, 'CIT-222-002-2020', 'BCT', 'Absent', '2024-11-03', 'BCT 2411'),
+(543, 'CIT-222-001-2020', 'BCT', 'Absent', '2024-11-03', 'BCT 2411'),
+(544, 'CIT-222-005-2020', 'BCT', 'Absent', '2024-11-03', 'BCT 2411'),
+(545, 'CIT-222-004-2020', 'BCT', 'Absent', '2024-11-03', 'BCT 2411'),
+(546, 'CIT-222-006-2020', 'BCT', 'Absent', '2024-11-03', 'BCT 2411'),
+(547, 'CIT-222-007-2020', 'BCT', 'Absent', '2024-11-03', 'BCT 2411');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblcourse`
+-- Structure de la table `tblcourse`
 --
 
 CREATE TABLE `tblcourse` (
@@ -83,7 +127,7 @@ CREATE TABLE `tblcourse` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `tblcourse`
+-- Déchargement des données de la table `tblcourse`
 --
 
 INSERT INTO `tblcourse` (`ID`, `name`, `facultyID`, `dateCreated`, `courseCode`) VALUES
@@ -100,7 +144,7 @@ INSERT INTO `tblcourse` (`ID`, `name`, `facultyID`, `dateCreated`, `courseCode`)
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblfaculty`
+-- Structure de la table `tblfaculty`
 --
 
 CREATE TABLE `tblfaculty` (
@@ -111,7 +155,7 @@ CREATE TABLE `tblfaculty` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `tblfaculty`
+-- Déchargement des données de la table `tblfaculty`
 --
 
 INSERT INTO `tblfaculty` (`Id`, `facultyName`, `facultyCode`, `dateRegistered`) VALUES
@@ -126,7 +170,7 @@ INSERT INTO `tblfaculty` (`Id`, `facultyName`, `facultyCode`, `dateRegistered`) 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbllecture`
+-- Structure de la table `tbllecture`
 --
 
 CREATE TABLE `tbllecture` (
@@ -141,19 +185,20 @@ CREATE TABLE `tbllecture` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `tbllecture`
+-- Déchargement des données de la table `tbllecture`
 --
 
 INSERT INTO `tbllecture` (`Id`, `firstName`, `lastName`, `emailAddress`, `password`, `phoneNo`, `facultyCode`, `dateCreated`) VALUES
 (22, 'frank', 'Tech', 'tech@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', '07123456789', 'CIT', '2024-05-02'),
 (16, 'John', 'Mark', 'john@gmail.com', '1f431fac09e825fc2e5d03fa5ed0b42d', '07123456789', 'FoSST', '2024-04-07'),
 (15, 'mark', 'lila', 'mark@gmail.com', '15c1469d0495004588968a3bde955397', '07123456789', 'CIT', '2024-04-07'),
-(14, 'Francis', 'Njenga', 'francis@gmail.com', '72fff2824f94462c2eaaa739b6e7d496', '07123456789', 'CIT', '2024-04-07');
+(14, 'Francis', 'Njenga', 'francis@gmail.com', '72fff2824f94462c2eaaa739b6e7d496', '07123456789', 'CIT', '2024-04-07'),
+(23, 'Modibo', 'Keita', 'keita@gmail.com', 'e69113e2ce8a1ea8515d3b04d961582a', '93909081', 'FENG', '2024-11-03');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblstudents`
+-- Structure de la table `tblstudents`
 --
 
 CREATE TABLE `tblstudents` (
@@ -166,24 +211,29 @@ CREATE TABLE `tblstudents` (
   `courseCode` varchar(20) NOT NULL,
   `studentImage1` varchar(300) NOT NULL,
   `studentImage2` varchar(300) NOT NULL,
-  `dateRegistered` varchar(50) NOT NULL
+  `dateRegistered` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `tblstudents`
+-- Déchargement des données de la table `tblstudents`
 --
 
-INSERT INTO `tblstudents` (`Id`, `firstName`, `lastName`, `registrationNumber`, `email`, `faculty`, `courseCode`, `studentImage1`, `studentImage2`, `dateRegistered`) VALUES
-(3, 'John', 'Macharia', 'CIT-222-003-2020', 'john@gmail.com', 'CIT', 'BCT', 'CIT-222-003-2020_image1.png', 'CIT-222-003-2020_image2.png', '2024-04-09'),
-(2, 'Mercy', 'Wambui', 'CIT-222-002-2020', 'mercy@gmail.com', 'CIT', 'BCT', 'CIT-222-002-2020_image1.png', 'CIT-222-002-2020_image2.png', '2024-04-09'),
-(1, 'Kevin', 'Ochieng', 'CIT-222-001-2020', 'kevin@gmail.com', 'CIT', 'BCT', 'CIT-222-001-2020_image1.png', 'CIT-222-001-2020_image2.png', '2024-04-09'),
-(123, 'student1', 'testing', 'CIT-222-005-2020', 'student1@gmail.com', 'CIT', 'BCT', 'CIT-222-005-2020_image1.png', 'CIT-222-005-2020_image2.png', '2024-05-02'),
-(4, 'frank', 'Tech', 'CIT-222-004-2020', 'tech@gmail.com', 'CIT', 'BCT', 'CIT-222-004-2020_image1.png', 'CIT-222-004-2020_image2.png', '2024-04-26');
+INSERT INTO `tblstudents` (`Id`, `firstName`, `lastName`, `registrationNumber`, `email`, `faculty`, `courseCode`, `studentImage1`, `studentImage2`, `dateRegistered`, `password`) VALUES
+(3, 'John', 'Macharia', 'CIT-222-003-2020', 'john@gmail.com', 'CIT', 'BCT', 'CIT-222-003-2020_image1.png', 'CIT-222-003-2020_image2.png', '2024-04-09', ''),
+(2, 'Mercy', 'Wambui', 'CIT-222-002-2020', 'mercy@gmail.com', 'CIT', 'BCT', 'CIT-222-002-2020_image1.png', 'CIT-222-002-2020_image2.png', '2024-04-09', ''),
+(1, 'Kevin', 'Ochieng', 'CIT-222-001-2020', 'kevin@gmail.com', 'CIT', 'BCT', 'CIT-222-001-2020_image1.png', 'CIT-222-001-2020_image2.png', '2024-04-09', ''),
+(123, 'student1', 'testing', 'CIT-222-005-2020', 'student1@gmail.com', 'CIT', 'BCT', 'CIT-222-005-2020_image1.png', 'CIT-222-005-2020_image2.png', '2024-05-02', ''),
+(4, 'frank', 'Tech', 'CIT-222-004-2020', 'tech@gmail.com', 'CIT', 'BCT', 'CIT-222-004-2020_image1.png', 'CIT-222-004-2020_image2.png', '2024-04-26', ''),
+(124, 'Modibo', 'Keita', 'CIT-2020-39-90', 'keita@gmail.com', 'FENG', 'BSE', 'CIT-2020-39-90_image1.png', 'CIT-2020-39-90_image2.png', '2024-11-01', 'e69113e2ce8a1ea8515d3b04d961582a'),
+(125, 'Gaoussou', 'Tangara', 'CIT-222-006-2020', 'modibokeita@9390gmail.com', 'CIT', 'BCT', 'CIT-222-006-2020_image1.png', 'CIT-222-006-2020_image2.png', '2024-11-01', '$2y$10$/W40BrxjFCrl15Zx/LaEOOQ/hQBWy0MJ7/f5FDRALWySuCynTbyya'),
+(126, 'Mock', 'Keita', '202234640', 'keita@gmail.com', 'FENG', 'BSE', '202234640_image1.png', '202234640_image2.png', '2024-11-03', 'password'),
+(127, 'Moussa', 'Coulibaly', 'CIT-222-007-2020', 'coulibaly@gmail.com', 'CIT', 'BCT', 'CIT-222-007-2020_image1.png', 'CIT-222-007-2020_image2.png', '2024-11-03', 'password');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblunit`
+-- Structure de la table `tblunit`
 --
 
 CREATE TABLE `tblunit` (
@@ -195,7 +245,7 @@ CREATE TABLE `tblunit` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `tblunit`
+-- Déchargement des données de la table `tblunit`
 --
 
 INSERT INTO `tblunit` (`ID`, `name`, `unitCode`, `courseID`, `dateCreated`) VALUES
@@ -209,7 +259,7 @@ INSERT INTO `tblunit` (`ID`, `name`, `unitCode`, `courseID`, `dateCreated`) VALU
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblvenue`
+-- Structure de la table `tblvenue`
 --
 
 CREATE TABLE `tblvenue` (
@@ -223,116 +273,116 @@ CREATE TABLE `tblvenue` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `tblvenue`
+-- Déchargement des données de la table `tblvenue`
 --
 
 INSERT INTO `tblvenue` (`ID`, `className`, `facultyCode`, `currentStatus`, `capacity`, `classification`, `dateCreated`) VALUES
-(4, 'B 06', 'VT01', 'available', 100, 'class', '2024-04-07'),
+(4, 'B 06', 'CIT', 'available', 100, 'class', '2024-04-07'),
 (5, 'B 08', 'CIT', 'available', 80, 'class', '2024-04-07'),
 (6, 'Korea 4', 'CIT', 'available', 80, 'laboratory', '2024-04-07'),
 (7, 'Lecture Hall LH01', 'FAMECO', 'available', 200, 'lectureHall', '2024-04-07'),
 (8, 'LH 4', 'MSC', 'available', 100, 'computerLab', '2024-04-09');
 
 --
--- Indexes for dumped tables
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `tbladmin`
+-- Index pour la table `tbladmin`
 --
 ALTER TABLE `tbladmin`
   ADD PRIMARY KEY (`Id`);
 
 --
--- Indexes for table `tblattendance`
+-- Index pour la table `tblattendance`
 --
 ALTER TABLE `tblattendance`
   ADD PRIMARY KEY (`attendanceID`);
 
 --
--- Indexes for table `tblcourse`
+-- Index pour la table `tblcourse`
 --
 ALTER TABLE `tblcourse`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indexes for table `tblfaculty`
+-- Index pour la table `tblfaculty`
 --
 ALTER TABLE `tblfaculty`
   ADD PRIMARY KEY (`Id`);
 
 --
--- Indexes for table `tbllecture`
+-- Index pour la table `tbllecture`
 --
 ALTER TABLE `tbllecture`
   ADD PRIMARY KEY (`Id`);
 
 --
--- Indexes for table `tblstudents`
+-- Index pour la table `tblstudents`
 --
 ALTER TABLE `tblstudents`
   ADD PRIMARY KEY (`Id`);
 
 --
--- Indexes for table `tblunit`
+-- Index pour la table `tblunit`
 --
 ALTER TABLE `tblunit`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indexes for table `tblvenue`
+-- Index pour la table `tblvenue`
 --
 ALTER TABLE `tblvenue`
   ADD PRIMARY KEY (`ID`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `tbladmin`
+-- AUTO_INCREMENT pour la table `tbladmin`
 --
 ALTER TABLE `tbladmin`
   MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `tblattendance`
+-- AUTO_INCREMENT pour la table `tblattendance`
 --
 ALTER TABLE `tblattendance`
-  MODIFY `attendanceID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=483;
+  MODIFY `attendanceID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=548;
 
 --
--- AUTO_INCREMENT for table `tblcourse`
+-- AUTO_INCREMENT pour la table `tblcourse`
 --
 ALTER TABLE `tblcourse`
   MODIFY `ID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
--- AUTO_INCREMENT for table `tblfaculty`
+-- AUTO_INCREMENT pour la table `tblfaculty`
 --
 ALTER TABLE `tblfaculty`
   MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT for table `tbllecture`
+-- AUTO_INCREMENT pour la table `tbllecture`
 --
 ALTER TABLE `tbllecture`
-  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
--- AUTO_INCREMENT for table `tblstudents`
+-- AUTO_INCREMENT pour la table `tblstudents`
 --
 ALTER TABLE `tblstudents`
-  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=124;
+  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=128;
 
 --
--- AUTO_INCREMENT for table `tblunit`
+-- AUTO_INCREMENT pour la table `tblunit`
 --
 ALTER TABLE `tblunit`
   MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT for table `tblvenue`
+-- AUTO_INCREMENT pour la table `tblvenue`
 --
 ALTER TABLE `tblvenue`
   MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
@@ -341,4 +391,3 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="logo.jpg" type="image/x-icon" />
+    <link rel="stylesheet" href="home.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Barlow:wght@100;300&family=Kdam+Thmor+Pro&family=Lato:wght@700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Barlow:wght@100;300&family=Gentium+Plus&family=Kdam+Thmor+Pro&family=Lato:wght@700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Barlow:wght@100;300&family=Gentium+Plus&family=Kdam+Thmor+Pro&family=Lato:wght@700&family=Source+Sans+Pro&display=swap" rel="stylesheet">
+<title>LOGIN</title>
+  </head>
+  <body>
+    <img src="face.gif" alt="" width="200px" height="200px" class='face'>
+<div class="heading">
+  Attendance System 
+</div>
+
+       <div class="box">
+        <div class=""></div>
+        <div class=""></div>
+        <div class=""></div>
+        <div class=""></div>
+        <div class=""></div>
+        <div class=""></div>
+        <div class=""></div>
+        <div class=""></div>
+        <div class=""></div>
+        <div class=""></div>
+        <div class=""></div>
+        <div class=""></div>
+        <div class=""></div>
+      </div>
+<div class="btns">
+  <button class="button-56" role="button"><a href="Admin/login.php" class="nonet">Login As Admin</a></button><br>
+  <button class="button-56" role="button"><a href="Lecture/login.php" class="nonet">Login As Professor</a></button><br>
+  <button class="button-56" role="button"><a href="Student/login.php" class="nonet">Login As Student </a></button>
+</div>
+
+</html>
